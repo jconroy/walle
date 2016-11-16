8138c18778c597050e4d13717be2409470f01239
Jason Conroy <jason@findingsimple.com>
2016-11-16 14:22:35 +1100

    Clean up test code for queues

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use App\Jobs\ExampleJob;
 
 $app->get('/', function () use ($app) {
-    return $app->version();
+	//dispatch(new ExampleJob);
+    //return $app->version();
+    return 'WALL-E';
 });
